0f47f9e3a051565b216b17f52dc099d04b76e7e5
KaiSiMai <51234911+KaiSiMai@users.noreply.github.com>
2021-02-26 23:55:16 +0800

    Update dkd_coin.js

--- dkd_coin.js
+++ dkd_coin.js
@@ -390,13 +390,13 @@ let url = {
         url : 'http://dkd-api.dysdk.com/money/withdraw_do?'+dkdbody,
         headers : JSON.parse(dkdhd),
        // body : dkdtxbody,}
-       body : `{
-        "money":1,
-        "type":2,
-        "withdraw_card":null,
-       "program":8,
-       "is_special":1
-      }`,
+       body : {
+        money:1,
+        type:2,
+        withdraw_card:null,
+       program:8,
+       is_special:1
+      },
 }
 $.post(url, async (err, resp, data) => {
         try {
